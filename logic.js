32236e3e5da704e8a28944e9ac123dd3ddb06067
dansmith09 <dansmith_09@hotmail.com>
2022-06-16 23:50:47 +1000

    Added functionality if user doesn't input a date

--- logic.js
+++ logic.js
@@ -8,24 +8,26 @@ var userSelectedCrypto = '';
 
 
 function handleconfirmButton() {
-    userSelectedDate = dateInput.value;
-    userSelectedCrypto = cryptoOptionsSelect.value;
+  userSelectedDate = dateInput.value;
+  userSelectedCrypto = cryptoOptionsSelect.value;
 
-    var requestURL = `https://api.exchangerate.host/convert?source=crypto&from=${userSelectedCrypto}&to=USD&places=2&date=${userSelectedDate}`;
+  var requestURL = `https://api.exchangerate.host/convert?source=crypto&from=${userSelectedCrypto}&to=USD&places=2&date=${userSelectedDate}`;
 
-    fetch(requestURL)
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (data) {
-      console.log(data);
-      var priceOfCrypto = data.result ;
-      if (priceOfCrypto == 0) {
-        outputH3.textContent = "Sorry. There is a bit of an issue... either your selected currency is $0 (we hope not) or there is a problem with our system. Please try again later.";
-      } else {
-        outputH3.textContent = `On ${userSelectedDate} the price of ${userSelectedCrypto} was $` + priceOfCrypto + '!';
-      }
-    });
+  fetch(requestURL)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+    var priceOfCrypto = data.result ;
+    if (priceOfCrypto == 0) {
+      outputH3.textContent = "Sorry. There is a bit of an issue... either your selected currency is $0 (we hope not) or there is a problem with our system. Please try again later.";
+    } else if (userSelectedDate == '') {
+      outputH3.textContent = `As of today, the price of ${userSelectedCrypto} is $` + priceOfCrypto + '!';
+    } else {
+      outputH3.textContent = `On ${userSelectedDate} the price of ${userSelectedCrypto} was $` + priceOfCrypto + '!';
+    }
+  });
 
 }
 
